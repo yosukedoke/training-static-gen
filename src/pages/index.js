5f3fec52c31225e689eedee49b448f627a30abd7
yosukedoke <yosukedoke@ydniku.local>
2020-06-28 21:55:26 +0900

    QraphQLから画像を読み込むように修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,53 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default function Home() {
+export const query = graphql`
+query {
+  hero: file(relativePath: {eq: "hero.jpg"}) {
+    relativePath
+    childImageSharp {
+      fluid(maxWidth: 1600) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+  fruit: file(relativePath: {eq: "fruit.jpg"}) {
+    relativePath
+    childImageSharp {
+      fluid(maxWidth: 320) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+  grain: file(relativePath: {eq: "grain.jpg"}) {
+    relativePath
+    childImageSharp {
+      fluid(maxWidth: 320) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+  beverage: file(relativePath: {eq: "beverage.jpg"}) {
+    relativePath
+    childImageSharp {
+      fluid(maxWidth: 320) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+  berry: file(relativePath: {eq: "berry.jpg"}) {
+    relativePath
+    childImageSharp {
+      fluid(maxWidth: 1600) {
+        ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+}
+`
+
+export default function Home({ data }) {
   return (<div>
     <header className="header">
       <div className="container">
@@ -21,7 +68,7 @@ export default function Home() {
 
     <section className="hero">
       <figure>
-        <img src="/images/hero.jpg" alt="" />
+        <Img fluid={data.hero.childImageSharp.fluid} alt="" />
       </figure>
       <div className="catch">
         <h1>There is no love sincerer than<br /> the love of food.</h1>
@@ -40,7 +87,7 @@ export default function Home() {
         <div className="details">
           <div className="detail">
             <figure>
-              <img src="/images/fruit.jpg" alt="" />
+              <Img fluid={data.fruit.childImageSharp.fluid} alt="" />
             </figure>
             <h3>フルーツ</h3>
             <p>FRUIT</p>
@@ -49,7 +96,7 @@ export default function Home() {
 
           <div className="detail">
             <figure>
-              <img src="/images/grain.jpg" alt="" />
+              <Img fluid={data.grain.childImageSharp.fluid} alt="" />
             </figure>
             <h3>穀物</h3>
             <p>GRAIN</p>
@@ -58,7 +105,7 @@ export default function Home() {
 
           <div className="detail">
             <figure>
-              <img src="/images/beverage.jpg" alt="" />
+              <Img fluid={data.beverage.childImageSharp.fluid} alt="" />
             </figure>
             <h3>飲み物</h3>
             <p>BEVERAGE</p>
@@ -71,7 +118,7 @@ export default function Home() {
     <section className="photo">
       <h2 className="sr-only">Photo</h2>
       <figure>
-        <img src="/images/berry.jpg" alt="赤く熟したベリー" />
+        <Img fluid={data.berry.childImageSharp.fluid} alt="赤く熟したベリー" />
       </figure>
     </section>
 
