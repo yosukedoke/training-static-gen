01b4878c13830ede5d44fa103af1f97aaa77b85a
yosukedoke <yosukedoke@ydniku.local>
2020-06-29 12:13:07 +0900

    meta情報等を設定するMetaInfoコンポーネントを実装

---
+++ src/components/meta-data.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const MetaData = () => (
+  <Helmet>
+    <html lang="ja" />
+    <title>ESSENTIAL</title>
+    <meta name="description" content="デスクリプション" />
+  </Helmet>
+)
+
+export default MetaData
