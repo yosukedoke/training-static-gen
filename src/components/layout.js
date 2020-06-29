ee62b7205f002c0ddc3144982e2cd8e2207812b3
yosukedoke <yosukedoke@ydniku.local>
2020-06-29 11:06:15 +0900

    Layoutコンポーネントにくくりだす

---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+const Layout = ({ children }) => (
+  <div>
+    <Header />
+
+    {children}
+
+    <Footer />
+  </div>
+)
+
+export default Layout
